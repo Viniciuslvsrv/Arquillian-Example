45c51f6daafa6eb05a7ed76372f2dace5aa15f3a
Vinicius Alves Saraiva <vinicius.saraiva@seatecnologia.com.br>
2024-08-17 11:00:53 -0300

    resolved package log4j

--- modules/arquillian-test-example-project/arquillian-test-example-project-test/src/testIntegration/java/com/arquillian/test/example/project/resource/v1_0/test/StepsResourceTest.java
+++ modules/arquillian-test-example-project/arquillian-test-example-project-test/src/testIntegration/java/com/arquillian/test/example/project/resource/v1_0/test/StepsResourceTest.java
@@ -1,21 +1,23 @@
 package com.arquillian.test.example.project.resource.v1_0.test;
 
-import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.portal.kernel.service.UserLocalService;
-import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.test.rule.Inject;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.apache.log4j.Appender;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DataGuard;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.Inject;
 
 /**
  * @author vinicius
  */
+
+@DataGuard(scope = DataGuard.Scope.METHOD)
 @RunWith(Arquillian.class)
 public class StepsResourceTest {
 
